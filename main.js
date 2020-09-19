bcfa84d8772a58dac0f7efd10d8952521f1e79b0
mrcau <mrcau@naver.com>
2020-09-20 08:58:27 +0900

    CSS : scroll

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const toggle=document.querySelector('.navbar__toggle');
-const menu=document.querySelector('.navbar__menu');
 const navbar=document.querySelector('#navbar');
+const contact=document.querySelector('#contact');
+
 // navbar scroll view
 const navbarHeight=navbar.getBoundingClientRect().height;
 document.addEventListener('scroll',()=>{
@@ -12,5 +13,18 @@ document.addEventListener('scroll',()=>{
     else{
         navbar.classList.remove('dark');
     }
+});
+
+// Handle scrolling
+const menu=document.querySelector('.navbar__menu');
+menu.addEventListener('click',(event)=>{
+    const link = event.target.dataset.link;
+    if(link==null){
+        return;
+    }
+    console.log(link);
+    const scrollTo=document.querySelector(link);
+    console.log(scrollTo);
+    scrollTo.scrollIntoView({behavior:"smooth"});
 });
 
